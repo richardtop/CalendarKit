52a3cf528b0409ed82e3d689b1ff7c4023ef216c
Richard <richard@hyper.no>
2015-10-20 16:56:26 +0200

    Create DayView

--- Source/DayView.swift
+++ Source/DayView.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class DayView: UIView {
 
+  let timelinePager = PagingScrollView()
+
   override init(frame: CGRect) {
     super.init(frame: frame)
     configure()
@@ -13,7 +15,32 @@ class DayView: UIView {
   }
 
   func configure() {
-
+    configureTimelinePager()
   }
 
+  func configureTimelinePager() {
+    for _ in 0...2 {
+      print("asd")
+      let timeline = TimelineView(frame: bounds)
+      timeline.frame.size.height = timeline.fullHeight
+
+      let verticalScrollView = UIScrollView(frame: bounds)
+      verticalScrollView.addSubview(timeline)
+      verticalScrollView.contentSize = timeline.frame.size
+
+      timelinePager.addSubview(verticalScrollView)
+      timelinePager.reusableViews.append(verticalScrollView)
+    }
+
+    let contentWidth = CGFloat(timelinePager.reusableViews.count) * bounds.width
+    let contentHeight = timelinePager.reusableViews.first!.bounds.height
+    let size = CGSize(width: contentWidth, height: contentHeight)
+    timelinePager.contentSize = size
+
+    addSubview(timelinePager)
+  }
+
+  override func layoutSubviews() {
+    timelinePager.frame = bounds
+  }
 }
