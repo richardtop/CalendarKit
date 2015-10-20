21942a6fdb82e2768e72d3890a8a83b987952ee2
Richard <richard@hyper.no>
2015-10-20 16:57:18 +0200

    Create DayViewController

--- Source/DayViewController.swift
+++ Source/DayViewController.swift
@@ -2,10 +2,12 @@ import UIKit
 
 public class DayViewController: UIViewController {
 
-  lazy var dayView: DayView = DayView()
+//  lazy var dayView: DayView = DayView()
 
   override public func viewDidLoad() {
     super.viewDidLoad()
+
+    let dayView = DayView(frame: view.bounds)
 
     dayView.frame = view.bounds
     view.addSubview(dayView)
