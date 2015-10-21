87446622f3264254f8aadf97fd5bee08a15bb161
Richard <richard@hyper.no>
2015-10-21 17:36:58 +0200

    Remove debugging code

--- Source/Extensions/Array+Shift.swift
+++ Source/Extensions/Array+Shift.swift
@@ -12,9 +12,7 @@ extension Array {
   // TODO: refactor this function while keeping the interface the same
   mutating func shift(index: Int) {
     if index == 0 {return}
-    print(index)
     for _ in 1...abs(index) {
-      print("applemusic")
       if index > 0 {
         shiftRight()
       } else {
