a8fa199efdb5d60ee7b0351c0d1d4ba0c3686c52
Richard <richard@hyper.no>
2015-10-22 15:06:43 +0200

    Add navigation controller

--- CalendarKitDemo/CalendarKitDemo/AppDelegate.swift
+++ CalendarKitDemo/CalendarKitDemo/AppDelegate.swift
@@ -4,17 +4,18 @@ import CalendarKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+  var window: UIWindow?
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        window?.backgroundColor = UIColor.whiteColor()
-        window?.makeKeyAndVisible()
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    window = UIWindow(frame: UIScreen.mainScreen().bounds)
+    window?.backgroundColor = UIColor.whiteColor()
+    window?.makeKeyAndVisible()
 
-        let vc = DayViewController()
-        window?.rootViewController = vc
-        
-        return true
-    }
+    let vc = DayViewController()
+    let navigationController = UINavigationController(rootViewController: vc)
+    window?.rootViewController = navigationController
+
+    return true
+  }
 }
 
