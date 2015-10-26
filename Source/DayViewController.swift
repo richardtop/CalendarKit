bc3b9483dffb67294a12592aef40f30fb54aeaa7
Richard <richard@hyper.no>
2015-10-26 11:54:34 +0100

    Create mock events for datasource request

--- Source/DayViewController.swift
+++ Source/DayViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import DateTools
 
 public class DayViewController: UIViewController {
 
@@ -9,9 +10,22 @@ public class DayViewController: UIViewController {
     self.edgesForExtendedLayout = UIRectEdge.None
     view.addSubview(dayView)
     view.tintColor = UIColor.redColor()
+
+    dayView.dataSource = self
   }
 
   public override func viewDidLayoutSubviews() {
     dayView.fillSuperview()
+  }
+}
+
+extension DayViewController: DayViewDataSource {
+  func eventViewsForDate(date: NSDate) -> [EventView] {
+
+    let datePeriod = DTTimePeriod(size: .Hour, startingAt: date)
+    let eventView = EventView()
+    eventView.datePeriod = datePeriod
+
+    return [eventView]
   }
 }
