a698a404f0318cb402945fa74a89da16bcedbfc3
LinnierGames <esericksanc@gmail.com>
2018-04-28 23:14:29 -0700

    WIP: Populate ScrollView
    BUG: vertical stack view is matching the scrollview’s frame.height vs
    the contentSize.height
    
    TODO: add `All-Day` label
    TODO: remove local vars by using properties
    TODO: use reusable event views

--- Source/Timeline/AllDayView.swift
+++ Source/Timeline/AllDayView.swift
@@ -1,10 +1,3 @@
-//
-//  AllDayView.swift
-//  workspace
-//
-//  Created by Erick Sanchez on 4/28/18.
-//  Copyright © 2018 LinnierGames. All rights reserved.
-//
 
 import UIKit
 
@@ -34,7 +27,7 @@ public class AllDayView: UIView {
   // MARK: - METHODS
   
   private func configure() {
-    
+    backgroundColor = UIColor.gray
     reloadData()
   }
   
@@ -43,24 +36,72 @@ public class AllDayView: UIView {
       return
     }
     
+    // clear subviews TODO: clear out only contents of scroll view
+    subviews.forEach { $0.removeFromSuperview() }
+    
     let nEventDescriptors = dataSource.numberOfAllDayEvents(in: self)
     if nEventDescriptors == 0 || nEventDescriptors < 0 { return }
     
-    // create vertical stack view
+    //TODO: add All-Day UILabel
     
-    for index in 0...nEventDescriptors {
+    //TODO: remove local vars by using properties
+    let allDayLabelWidth: CGFloat = 53.0
+    let scrollViewWidth: CGFloat = bounds.width - allDayLabelWidth
+    let eventViewHeight: CGFloat = 32.0
+    
+    // create vertical stack view
+    let verticalStackView = UIStackView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+    verticalStackView.distribution = .fillEqually
+    verticalStackView.axis = .vertical
+    verticalStackView.spacing = 1.0
+    var horizontalStackView: UIStackView! = nil
+    
+    for index in 0...nEventDescriptors - 1 {
       let eventDescriptor = dataSource.allDayView(self, eventDescriptorFor: index)
       
       // create event TODO: reuse event views
+      let eventRect = CGRect(x: 0, y: 0, width: 10, height: 10)
+      let eventView = EventView(frame: eventRect)
+      eventView.updateWithDescriptor(event: eventDescriptor)
       
       // create horz stack view if index % 2 == 0
+      if index % 2 == 0 {
+        horizontalStackView = UIStackView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+        horizontalStackView.axis = .horizontal
+        horizontalStackView.distribution = .fillEqually
+        horizontalStackView.spacing = 1.0
+        verticalStackView.addArrangedSubview(horizontalStackView)
+      }
       
       // add eventView to horz. stack view
+      horizontalStackView.addArrangedSubview(eventView)
     }
     
-    // create scroll view, vert. stack view inside, update content view
+    let nHorizontalStackViews: Int = nEventDescriptors / 2
     
-    // addSubview(scrollview)
+    // create scroll view, vert. stack view inside, pin vert. stack view, update content view by the number of horz. stack views
+    let scrollViewHeight = min(eventViewHeight * 2 + eventViewHeight * 0.5, CGFloat(nHorizontalStackViews) * eventViewHeight)
+    let scrollview = UIScrollView(frame: CGRect(x: allDayLabelWidth, y: 0, width: scrollViewWidth, height: scrollViewHeight))
+    scrollview.isScrollEnabled = true
+    scrollview.alwaysBounceVertical = true
+    scrollview.addSubview(verticalStackView)
+    verticalStackView.translatesAutoresizingMaskIntoConstraints = false
+    verticalStackView.trailingAnchor.constraint(equalTo: scrollview.trailingAnchor, constant: 0).isActive = true
+    verticalStackView.topAnchor.constraint(equalTo: scrollview.topAnchor, constant: 0).isActive = true
+    verticalStackView.leadingAnchor.constraint(equalTo: scrollview.leadingAnchor, constant: 0).isActive = true
+    verticalStackView.bottomAnchor.constraint(equalTo: scrollview.bottomAnchor, constant: 0).isActive = true
+    verticalStackView.widthAnchor.constraint(equalTo: scrollview.widthAnchor, multiplier: 1).isActive = true
+    verticalStackView.heightAnchor.constraint(equalToConstant: CGFloat(nHorizontalStackViews) * (eventViewHeight + 1))
+    scrollview.heightAnchor.constraint(equalTo: verticalStackView.heightAnchor, multiplier: 1).isActive = true
+//    verticalStackView.heightAnchor.constraint(equalTo: scrollview.heightAnchor, multiplier: 1).isActive = true
+//    scrollview.contentSize = CGSize(width: scrollViewWidth, height: )
+    
+    addSubview(scrollview)
+    scrollview.translatesAutoresizingMaskIntoConstraints = false
+    scrollview.leftAnchor.constraint(equalTo: leftAnchor, constant: allDayLabelWidth).isActive = true
+    scrollview.topAnchor.constraint(equalTo: topAnchor, constant: 0.0).isActive = true
+    scrollview.rightAnchor.constraint(equalTo: rightAnchor, constant: 0).isActive = true
+    scrollview.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0).isActive = true
   }
   
   // MARK: - IBACTIONS/IBOUTLETS
