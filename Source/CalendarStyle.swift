58e62c5c1553b97899aece1b6afb726c41f55026
Amelia1992 <45912102+Amelia1992@users.noreply.github.com>
2019-02-04 12:40:06 +0330

    Update CalendarStyle.swift

--- Source/CalendarStyle.swift
+++ Source/CalendarStyle.swift
@@ -105,9 +105,9 @@ public class TimelineStyle: NSCopying {
   public var dateStyle : DateStyle = .system
   public var eventsWillOverlap: Bool = false
   public var splitMinuteInterval: Int = 15
-  public var verticalDiff: CGFloat = 45
-  public var verticalInset: CGFloat = 10
-  public var leftInset: CGFloat = 53
+  public var verticalDiff: CGFloat = 90
+  public var verticalInset: CGFloat = 20
+  public var leftInset: CGFloat = 106
   public var eventGap: CGFloat = 0
   public init() {}
   public func copy(with zone: NSZone? = nil) -> Any {
