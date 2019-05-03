d25fc0633a2061ea83fc9a61498efa2f2d8a8f93
Richard Topchii <topchiy@protonmail.com>
2019-05-04 00:21:04 +0300

    Remove PagingScrollview completely from the project

--- Source/Timeline/TimelinePagerView.swift
+++ Source/Timeline/TimelinePagerView.swift
@@ -17,17 +17,15 @@ public class TimelinePagerView: UIView {
 
   public var timelineScrollOffset: CGPoint {
     // Any view is fine as they are all synchronized
-    return timelinePager.reusableViews.first?.contentOffset ?? CGPoint()
+    let offset = (pagingViewController.viewControllers?.first as? TimelineContainerController)?.container.contentOffset
+    return offset ?? CGPoint()
   }
 
   open var autoScrollToFirstEvent = false
 
-  let timelinePager = PagingScrollView<TimelineContainer>()
   var pagingViewController = UIPageViewController(transitionStyle: .scroll,
                                                   navigationOrientation: .horizontal,
                                                   options: nil)
-  var timelineSynchronizer: ScrollSynchronizer?
-
   var style = TimelineStyle()
 
   weak var state: DayViewState? {
@@ -50,66 +48,68 @@ public class TimelinePagerView: UIView {
   }
 
   func configure() {
-    configureTimelinePager()
+    let vc = configureTimelineController(date: Date())
+    pagingViewController.setViewControllers([vc], direction: .forward, animated: false, completion: nil)
+    pagingViewController.dataSource = self
+    pagingViewController.delegate = self
+    addSubview(pagingViewController.view!)
   }
 
   public func updateStyle(_ newStyle: TimelineStyle) {
     style = newStyle.copy() as! TimelineStyle
-    timelinePager.reusableViews.forEach{ timelineContainer in
-      let timeline = timelineContainer.timeline
-      timeline.updateStyle(style)
-      timeline.frame.size.height = timeline.fullHeight
-      timelineContainer.contentSize = timeline.frame.size
-      timelineContainer.backgroundColor = style.backgroundColor
-    }
+    pagingViewController.viewControllers?.forEach({ (timelineContainer) in
+      if let controller = timelineContainer as? TimelineContainerController {
+        self.updateStyleOfTimelineContainer(controller: controller)
+      }
+    })
+    pagingViewController.view.backgroundColor = style.backgroundColor
+  }
+
+  func updateStyleOfTimelineContainer(controller: TimelineContainerController) {
+    let container = controller.container
+    let timeline = controller.timeline
+    timeline.updateStyle(style)
+    container.backgroundColor = style.backgroundColor
   }
 
   public func timelinePanGestureRequire(toFail gesture: UIGestureRecognizer) {
-    for timelineContainer in timelinePager.reusableViews {
-      timelineContainer.panGestureRecognizer.require(toFail: gesture)
+    for controller in pagingViewController.viewControllers ?? [] {
+      if let controller = controller as? TimelineContainerController {
+        let container = controller.container
+        container.panGestureRecognizer.require(toFail: gesture)
+      }
     }
   }
 
   public func scrollTo(hour24: Float) {
     // Any view is fine as they are all synchronized
-    timelinePager.reusableViews.first?.scrollTo(hour24: hour24)
+    if let controller = pagingViewController.viewControllers?.first as? TimelineContainerController {
+      controller.container.scrollTo(hour24: hour24)
+    }
   }
 
-  func configureTimelinePager() {
-    var verticalScrollViews = [TimelineContainer]()
-    for i in -1...1 {
-      let timeline = TimelineView(frame: bounds)
-      timeline.delegate = self
-      timeline.eventViewDelegate = self
-      timeline.frame.size.height = timeline.fullHeight
-      timeline.date = Date().add(TimeChunk.dateComponents(days: i))
-
-      let verticalScrollView = TimelineContainer(timeline)
-      verticalScrollView.addSubview(timeline)
-      verticalScrollView.contentSize = timeline.frame.size
-
-      timelinePager.addSubview(verticalScrollView)
-      timelinePager.reusableViews.append(verticalScrollView)
-      verticalScrollViews.append(verticalScrollView)
-    }
-    timelineSynchronizer = ScrollSynchronizer(views: verticalScrollViews)
-    addSubview(timelinePager)
-
-    timelinePager.viewDelegate = self
+  func configureTimelineController(date: Date) -> TimelineContainerController {
+    let controller = TimelineContainerController()
+    updateStyleOfTimelineContainer(controller: controller)
+    let timeline = controller.timeline
+    timeline.delegate = self
+    timeline.eventViewDelegate = self
+    timeline.date = date.dateOnly()
+    updateTimeline(timeline)
+    return controller
   }
 
   public func reloadData() {
-    timelinePager.reusableViews.forEach{self.updateTimeline($0.timeline)}
+    pagingViewController.children.forEach({ (controller) in
+      if let controller = controller as? TimelineContainerController {
+        self.updateTimeline(controller.timeline)
+      }
+    })
   }
 
   override public func layoutSubviews() {
     super.layoutSubviews()
-
-    let contentWidth = CGFloat(timelinePager.reusableViews.count) * bounds.width
-    let size = CGSize(width: contentWidth, height: 50)
-    timelinePager.contentSize = size
-    timelinePager.contentOffset = CGPoint(x: bounds.width, y: 0)
-    timelinePager.fillSuperview()
+    pagingViewController.view.fillSuperview()
   }
 
   func updateTimeline(_ timeline: TimelineView) {
@@ -121,59 +121,58 @@ public class TimelinePagerView: UIView {
     let validEvents = events.filter{$0.datePeriod.overlaps(with: day)}
     timeline.layoutAttributes = validEvents.map(EventLayoutAttributes.init)
   }
+
+  func scrollToFirstEventIfNeeded() {
+    if autoScrollToFirstEvent {
+      if let controller = pagingViewController.viewControllers?.first as? TimelineContainerController {
+        controller.container.scrollToFirstEvent()
+      }
+    }
+  }
 }
 
 extension TimelinePagerView: DayViewStateUpdating {
   public func move(from oldDate: Date, to newDate: Date) {
     let oldDate = oldDate.dateOnly()
     let newDate = newDate.dateOnly()
+    let newController = configureTimelineController(date: newDate)
     if newDate.isEarlier(than: oldDate) {
-      var timelineDate = newDate.subtract(TimeChunk.dateComponents(days: 0))
-      for timelineContainer in timelinePager.reusableViews {
-        timelineContainer.timeline.date = timelineDate
-        timelineDate = timelineDate.add(TimeChunk.dateComponents(days: 1))
-        updateTimeline(timelineContainer.timeline)
-      }
-      timelinePager.scrollBackward()
+      pagingViewController.setViewControllers([newController], direction: .reverse, animated: true, completion: nil)
     } else if newDate.isLater(than: oldDate) {
-      var timelineDate = newDate.add(TimeChunk.dateComponents(days: 0))
-      for timelineContainer in timelinePager.reusableViews.reversed() {
-        timelineContainer.timeline.date = timelineDate
-        timelineDate = timelineDate.subtract(TimeChunk.dateComponents(days: 1))
-        updateTimeline(timelineContainer.timeline)
-      }
-      timelinePager.scrollForward()
+      pagingViewController.setViewControllers([newController], direction: .forward, animated: true, completion: nil)
     }
   }
 }
 
-
-extension TimelinePagerView: PagingScrollViewDelegate {
-  func scrollviewDidScrollToViewAtIndex(_ index: Int) {
-    let nextDate = timelinePager.reusableViews[index].timeline.date
-    delegate?.timelinePager(timelinePager: self, willMoveTo: nextDate)
-    state?.client(client: self, didMoveTo: nextDate)
-    scrollToFirstEventIfNeeded()
-    delegate?.timelinePager(timelinePager: self, didMoveTo: nextDate)
-
-    // Update left & right views
-
-    let leftView = timelinePager.reusableViews[0].timeline
-    let rightView = timelinePager.reusableViews[2].timeline
-
-    guard let state = state
-      else{ return }
-
-    leftView.date = state.selectedDate.add(TimeChunk.dateComponents(days: -1))
-    rightView.date = state.selectedDate.add(TimeChunk.dateComponents(days: 1))
-
-    [leftView, rightView].forEach{self.updateTimeline($0)}
+extension TimelinePagerView: UIPageViewControllerDataSource {
+  public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+    guard let containerController = viewController as? TimelineContainerController  else {return nil}
+    let previousDate = containerController.timeline.date.add(TimeChunk.dateComponents(days: -1))
+    let vc = configureTimelineController(date: previousDate)
+    let offset = (pageViewController.viewControllers?.first as? TimelineContainerController)?.container.contentOffset
+    vc.pendingContentOffset = offset
+    return vc
   }
 
-  func scrollToFirstEventIfNeeded() {
-    if autoScrollToFirstEvent {
-      let index = Int(timelinePager.currentScrollViewPage)
-      timelinePager.reusableViews[index].scrollToFirstEvent()
+  public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+    guard let containerController = viewController as? TimelineContainerController  else {return nil}
+    let nextDate = containerController.timeline.date.add(TimeChunk.dateComponents(days: 1))
+    let vc = configureTimelineController(date: nextDate)
+    let offset = (pageViewController.viewControllers?.first as? TimelineContainerController)?.container.contentOffset
+    vc.pendingContentOffset = offset
+    return vc
+  }
+}
+
+extension TimelinePagerView: UIPageViewControllerDelegate {
+  public func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+    guard completed else {return}
+    if let timelineContainerController = pageViewController.viewControllers?.first as? TimelineContainerController {
+      let selectedDate = timelineContainerController.timeline.date
+      delegate?.timelinePager(timelinePager: self, willMoveTo: selectedDate)
+      state?.client(client: self, didMoveTo: selectedDate)
+      scrollToFirstEventIfNeeded()
+      delegate?.timelinePager(timelinePager: self, didMoveTo: selectedDate)
     }
   }
 }
