e5ff2ecc44f20d16d1511bc6a5389cf2489d0605
Richard Topchii <topchiy@protonmail.com>
2019-05-31 05:54:30 +0300

    Use isEditing parameter to modify event's color

--- Source/Timeline/Event.swift
+++ Source/Timeline/Event.swift
@@ -8,15 +8,33 @@ open class Event: EventDescriptor {
   public var attributedText: NSAttributedString?
   public var color = UIColor.blue {
     didSet {
-      backgroundColor = color.withAlphaComponent(0.3)
-      var h: CGFloat = 0, s: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
-      color.getHue(&h, saturation: &s, brightness: &b, alpha: &a)
-      textColor = UIColor(hue: h, saturation: s, brightness: b * 0.4, alpha: a)
+      updateColors()
     }
   }
   public var backgroundColor = UIColor.blue.withAlphaComponent(0.3)
   public var textColor = UIColor.black
   public var font = UIFont.boldSystemFont(ofSize: 12)
   public var userInfo: Any?
+  public var isEditing: Bool = false {
+    didSet {
+      updateColors()
+    }
+  }
   public init() {}
+
+  private func updateColors() {
+    isEditing ? applyEditingColors() : applyStandardColors()
+  }
+
+  private func applyStandardColors() {
+    backgroundColor = color.withAlphaComponent(0.3)
+    var h: CGFloat = 0, s: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
+    color.getHue(&h, saturation: &s, brightness: &b, alpha: &a)
+    textColor = UIColor(hue: h, saturation: s, brightness: b * 0.4, alpha: a)
+  }
+
+  private func applyEditingColors() {
+    backgroundColor = color
+    textColor = .white
+  }
 }
