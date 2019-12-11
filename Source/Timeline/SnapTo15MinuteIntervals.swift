79ee8ad5ae10f3d9358ac9ce1ae6fb83ccf149cd
Richard Topchii <topchiy@protonmail.ch>
2019-12-11 21:12:38 +0200

    Simplify snapping behavior code

--- Source/Timeline/SnapTo15MinuteIntervals.swift
+++ Source/Timeline/SnapTo15MinuteIntervals.swift
@@ -2,13 +2,13 @@ import Foundation
 
 final class SnapTo15MinuteIntervals: EventEditingSnappingBehavior {
   private let calendar: Calendar
-  private let unit: Double = (60 / 4) / 2
 
   init(_ calendar: Calendar) {
     self.calendar = calendar
   }
-  
+
   func nearestDate(to date: Date) -> Date {
+    let unit: Double = (60 / 4) / 2
     var accHour = Int(accentedHour(for: date))
     let minute = Double(component(component: .minute, from: date))
     if (60 - unit)...59 ~= minute {
@@ -19,25 +19,19 @@ final class SnapTo15MinuteIntervals: EventEditingSnappingBehavior {
                          second: 0,
                          of: date)!
   }
-  
+
   func accentedHour(for date: Date) -> Int {
     return Int(component(component: .hour, from: date))
   }
 
   func accentedMinute(for date: Date) -> Int {
-    var accentedMinute = Double(component(component: .minute, from: date))
-    guard unit...(60 - unit) ~= accentedMinute else {
-      return 0
-    }
-
-    if (15 - unit)...(15 + unit) ~= accentedMinute {
-      accentedMinute = 15
-    } else if (30 - unit)...(30 + unit) ~= accentedMinute {
-      accentedMinute = 30
-    } else {
-      accentedMinute = 45
-    }
-    return Int(accentedMinute)
+    let accentedMinute = Double(component(component: .minute, from: date))
+    return snapTo15Minute(accentedMinute)
+  }
+  
+  private func snapTo15Minute(_ number: Double) -> Int {
+    let value = 15 * Int(round(number / 15.0))
+    return value < 60 ? value : 0
   }
   
   private func component(component: Calendar.Component, from date: Date) -> Int {
