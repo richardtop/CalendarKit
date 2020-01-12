526b32e6ffe3e402f28a5beec8ddb3b2d858ffb4
Richard Topchii <topchiy@protonmail.ch>
2020-01-12 23:28:06 +0200

    Update demo project

--- CalendarKitDemo/CalendarApp/CustomCalendarExampleController.swift
+++ CalendarKitDemo/CalendarApp/CustomCalendarExampleController.swift
@@ -211,7 +211,7 @@ class CustomCalendarExampleController: DayViewController, DatePickerControllerDe
     guard let descriptor = eventView.descriptor as? Event else {
       return
     }
-    
+    dayView.endEventEditing()
     print("Event has been longPressed: \(descriptor) \(String(describing: descriptor.userInfo))")
     dayView.beginEditing(event: descriptor, animated: true)
     print(Date())
@@ -232,7 +232,15 @@ class CustomCalendarExampleController: DayViewController, DatePickerControllerDe
   
   override func dayView(dayView: DayView, didLongPressTimelineAt date: Date) {
     print("Did long press timeline at date \(date)")
-    
+    // Cancel editing current event and start creating a new one
+    dayView.endEventEditing()
+    let event = generateEventNearDate(date)
+    print("Creating a new event")
+    dayView.create(event: event, animated: true)
+    createdEvent = event
+  }
+  
+  private func generateEventNearDate(_ date: Date) -> EventDescriptor {
     let duration = Int(arc4random_uniform(160) + 60)
     let startDate = date.subtract(TimeChunk.dateComponents(minutes: Int(CGFloat(duration) / 2)))
     let event = Event()
@@ -255,16 +263,14 @@ class CustomCalendarExampleController: DayViewController, DatePickerControllerDe
       event.textColor = textColorForEventInDarkTheme(baseColor: event.color)
       event.backgroundColor = event.color.withAlphaComponent(0.6)
     }
-    print("Creating a new event")
-    dayView.create(event: event, animated: true)
-    createdEvent = event
+    return event
   }
   
   override func dayView(dayView: DayView, didUpdate event: EventDescriptor) {
     print("did finish editing \(event)")
     print("new startDate: \(event.startDate) new endDate: \(event.endDate)")
     
-    if let edited = event.editedEvent {
+    if let _ = event.editedEvent {
       event.commitEditing()
     }
     
