d38b4cab795445258b8f1cd84cefdf993ad53243
Richard Topchii <topchiy@protonmail.ch>
2020-01-12 19:13:39 +0200

    Fix "all-day" string

--- Source/Timeline/AllDayView.swift
+++ Source/Timeline/AllDayView.swift
@@ -18,7 +18,7 @@ public class AllDayView: UIView {
   
   private lazy var textLabel: UILabel = {
     let label = UILabel(frame: CGRect(x: 8.0, y: 4.0, width: allDayLabelWidth, height: 24.0))
-    label.text = localizedString("allday")
+    label.text = localizedString("all-day")
     label.autoresizingMask = [.flexibleWidth]
     return label
   }()
