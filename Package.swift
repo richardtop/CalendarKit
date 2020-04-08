2eb2a86bb8056ebcf5b46ee2a5ee3968385ccd67
Richard Topchii <topchiy@protonmail.ch>
2020-04-08 18:25:39 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@
      dependencies: [
          // Dependencies declare other packages that this package depends on.
          // .package(url: /* package url */, from: "1.0.0"),
-         .package(url: "https://github.com/tomaculum/Neon.git", .branch("master")),
+         .package(url: "https://github.com/richardtop/Neon.git", .branch("master")),
          .package(url: "https://github.com/maniramezan/DateTools.git", .branch("mani_swiftpm"))
      ],
      targets: [
