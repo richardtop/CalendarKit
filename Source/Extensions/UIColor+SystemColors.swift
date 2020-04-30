1ad02dd06d6885342e5b0cd84ea6da74204b286c
Joshua T. Wood <joshuatw@gmail.com>
2020-04-30 14:02:50 +0200

    Support for iOS 13 system dark mode

--- Source/Extensions/UIColor+SystemColors.swift
+++ Source/Extensions/UIColor+SystemColors.swift
@@ -19,10 +19,22 @@ public enum SystemColors {
     }
     return .white
   }
+  public static var secondarySystemBackground: UIColor {
+    if #available(iOS 13, *) {
+      return .secondarySystemBackground
+    }
+    return UIColor(white: 247/255, alpha: 1)
+  }
   public static var systemRed: UIColor {
     if #available(iOS 13, *) {
       return .systemRed
     }
     return .red
+  }
+  public static var systemBlue: UIColor {
+    if #available(iOS 13, *) {
+      return .systemBlue
+    }
+    return .blue
   }
 }
