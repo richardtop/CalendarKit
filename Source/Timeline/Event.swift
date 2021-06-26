4996d599df9e066061ee750c81a5a1a0a8a74377
Richard Topchii <topchiy@protonmail.ch>
2021-06-26 03:16:29 +0300

    Add dark mode support to the default Event class

--- Source/Timeline/Event.swift
+++ Source/Timeline/Event.swift
@@ -49,16 +49,68 @@ public final class Event: EventDescriptor {
   private func updateColors() {
     (editedEvent != nil) ? applyEditingColors() : applyStandardColors()
   }
-
+  
+  /// Colors used when event is not in editing mode
   private func applyStandardColors() {
-    backgroundColor = color.withAlphaComponent(0.3)
+    backgroundColor = dynamicStandardBackgroundColor()
+    textColor = dynamicStandardTextColor()
+  }
+  
+  /// Colors used in editing mode
+  private func applyEditingColors() {
+    backgroundColor = color.withAlphaComponent(0.95)
+    textColor = .white
+  }
+  
+  /// Dynamic color that changes depending on the user interface style (dark / light)
+  private func dynamicStandardBackgroundColor() -> UIColor {
+    let light = backgroundColorForLightTheme(baseColor: color)
+    let dark = backgroundColorForDarkTheme(baseColor: color)
+    return dynamicColor(light: light, dark: dark)
+  }
+  
+  /// Dynamic color that changes depending on the user interface style (dark / light)
+  private func dynamicStandardTextColor() -> UIColor {
+    let light = textColorForLightTheme(baseColor: color)
+    let dark = textColorForDarkTheme(baseColor: color)
+    return dynamicColor(light: light, dark: dark)
+  }
+  
+  private func textColorForLightTheme(baseColor: UIColor) -> UIColor {
+    var h: CGFloat = 0, s: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
+    baseColor.getHue(&h, saturation: &s, brightness: &b, alpha: &a)
+    return UIColor(hue: h, saturation: s, brightness: b * 0.4, alpha: a)
+  }
+  
+  private func textColorForDarkTheme(baseColor: UIColor) -> UIColor {
+    var h: CGFloat = 0, s: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
+    baseColor.getHue(&h, saturation: &s, brightness: &b, alpha: &a)
+    return UIColor(hue: h, saturation: s * 0.3, brightness: b, alpha: a)
+  }
+  
+  private func backgroundColorForLightTheme(baseColor: UIColor) -> UIColor {
+    baseColor.withAlphaComponent(0.3)
+  }
+  
+  private func backgroundColorForDarkTheme(baseColor: UIColor) -> UIColor {
     var h: CGFloat = 0, s: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
     color.getHue(&h, saturation: &s, brightness: &b, alpha: &a)
-    textColor = UIColor(hue: h, saturation: s, brightness: b * 0.4, alpha: a)
+    return UIColor(hue: h, saturation: s, brightness: b * 0.4, alpha: a * 0.8)
   }
-
-  private func applyEditingColors() {
-    backgroundColor = color
-    textColor = .white
+  
+  private func dynamicColor(light: UIColor, dark: UIColor) -> UIColor {
+    if #available(iOS 13.0, *) {
+      return UIColor { traitCollection in
+        let interfaceStyle = traitCollection.userInterfaceStyle
+        switch interfaceStyle {
+        case .dark:
+          return dark
+        default:
+          return light
+        }
+      }
+    } else {
+      return light
+    }
   }
 }
