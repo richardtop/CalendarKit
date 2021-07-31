a32acc885e6ecf3c55b4ee30e80db182906dba40
Richard Topchii <topchiy@protonmail.ch>
2021-07-31 17:19:09 +0300

    Fix memory leak in CurrentTimeIndicator

--- Source/Timeline/CurrentTimeIndicator.swift
+++ Source/Timeline/CurrentTimeIndicator.swift
@@ -10,7 +10,7 @@ import UIKit
     }
   }
 
-  private var timer: Timer?
+  private weak var timer: Timer?
   @objc private func timerDidFire(_ sender: Timer) {
     date = Date()
   }
@@ -77,18 +77,23 @@ import UIKit
   }
   
   private func configureTimer() {
-    timer?.invalidate()
+    invalidateTimer()
     let date = Date()
     var components = calendar.dateComponents(Set([.era, .year, .month, .day, .hour, .minute]), from: date)
     components.minute! += 1
     let timerDate = calendar.date(from: components)!
-    timer = Timer(fireAt: timerDate,
+    let newTimer = Timer(fireAt: timerDate,
                   interval: 60,
                   target: self,
                   selector: #selector(timerDidFire(_:)),
                   userInfo: nil,
                   repeats: true)
-    RunLoop.current.add(timer!, forMode: .common)
+    RunLoop.current.add(newTimer, forMode: .common)
+    timer = newTimer
+  }
+  
+  private func invalidateTimer() {
+    timer?.invalidate()
   }
     
   private func updateDate() {
@@ -148,6 +153,15 @@ import UIKit
     default:
         is24hClock = Locale.autoupdatingCurrent.uses24hClock()
         break
+    }
+  }
+  
+  public override func willMove(toSuperview newSuperview: UIView?) {
+    super.willMove(toSuperview: newSuperview)
+    if newSuperview != nil {
+      configureTimer()
+    } else {
+      invalidateTimer()
     }
   }
 }
