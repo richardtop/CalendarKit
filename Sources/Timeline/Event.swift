a5e6f19a0fdab0e7aa79918885f3966f8dc95d2d
Richard Topchii <topchiy@protonmail.ch>
2021-11-21 04:24:53 +0200

    Update Event look in Dark mode

--- Sources/Timeline/Event.swift
+++ Sources/Timeline/Event.swift
@@ -72,20 +72,13 @@ public final class Event: EventDescriptor {
   /// Dynamic color that changes depending on the user interface style (dark / light)
   private func dynamicStandardTextColor() -> UIColor {
     let light = textColorForLightTheme(baseColor: color)
-    let dark = textColorForDarkTheme(baseColor: color)
-    return dynamicColor(light: light, dark: dark)
+    return dynamicColor(light: light, dark: color)
   }
   
   private func textColorForLightTheme(baseColor: UIColor) -> UIColor {
     var h: CGFloat = 0, s: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
     baseColor.getHue(&h, saturation: &s, brightness: &b, alpha: &a)
     return UIColor(hue: h, saturation: s, brightness: b * 0.4, alpha: a)
-  }
-  
-  private func textColorForDarkTheme(baseColor: UIColor) -> UIColor {
-    var h: CGFloat = 0, s: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
-    baseColor.getHue(&h, saturation: &s, brightness: &b, alpha: &a)
-    return UIColor(hue: h, saturation: s * 0.3, brightness: b, alpha: a)
   }
   
   private func backgroundColorForLightTheme(baseColor: UIColor) -> UIColor {
