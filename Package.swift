a9aa59ef1eb14635fc0e28fb9ca40e02cd9adb42
Richard Topchii <topchiy@protonmail.ch>
2023-03-20 22:05:10 +0200

    Bump swift-tools-version to 5.7

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
@@ -6,7 +6,7 @@ let package = Package(
     name: "CalendarKit",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v10),
+        .iOS(.v11),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
