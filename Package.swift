1d8cfbfae0e7f66cd6775e8ff1d2af78944f6910
Richard Topchii <topchiy@protonmail.ch>
2024-01-31 22:15:35 +0200

    Code cleanup, reformat example app

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
@@ -10,14 +9,11 @@ let package = Package(
         .tvOS(.v11)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "CalendarKit",
             targets: ["CalendarKit"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "CalendarKit",
                 path: "Sources")
     ]
